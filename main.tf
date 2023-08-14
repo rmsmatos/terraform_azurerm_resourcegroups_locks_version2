09a15b4b03e8e5aaf5ea6d025af21014481b3392
Rui Matos <rmsmatos@gmail.com>
2023-08-14 18:25:31 +0100

    First commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "azurerm_resourcegroup_version1" {
+  source     = "github.com/rmsmatos/terraform_azurerm_resourcegroup_version1"
+  name       = var.name
+  location   = var.location
+  managed_by = var.managed_by
+  tags       = var.tags
+}
+
+resource "azurerm_management_lock" "lock" {
+  name       = var.lock_name
+  scope      = module.azurerm_resourcegroup_version1.id
+  lock_level = var.lock_level
+  notes      = var.lock_notes
+}
